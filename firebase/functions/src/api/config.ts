42cd988caac2c55c03493759254e21696bbf5a97
sean1093 <sean1093@gmail.com>
2020-03-22 21:41:15 +0800

    add cors in API

--- firebase/functions/src/api/config.ts
+++ firebase/functions/src/api/config.ts
@@ -1,15 +1,21 @@
 import RECIPE from '../_data_/common.json';
-
+const cors = require('cors')({
+    origin: true
+});
 const { BASE_LIST, ATTACHED_LIST } = RECIPE;
 
 const getBaseList = (request: any, response: any) => {
     console.log(request);
-    response.send(BASE_LIST);
+    return cors(request, response, () => {
+        response.status(200).send(BASE_LIST);
+    });
 }
 
 const getAttachedList = (request: any, response: any) => {
     console.log(request);
-    response.send(ATTACHED_LIST);
+    return cors(request, response, () => {
+        response.status(200).send(ATTACHED_LIST);
+    });
 }
 
 const commonApi = {
--- firebase/functions/src/api/recipe.ts
+++ firebase/functions/src/api/recipe.ts
@@ -1,10 +1,15 @@
 import recipe from '../_data_/overpartylab-recipe.json';
-
+const cors = require('cors')({
+    origin: true
+});
 const { cocktails_list: cocktailsList } = recipe;
 
 const getCocktailsList = (request: any, response: any) => {
     console.log(request);
-    response.send(cocktailsList);
+    
+    return cors(request, response, () => {
+        response.status(200).send(cocktailsList);
+    });
 }
 
 const recipeApi = {
