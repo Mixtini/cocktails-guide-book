3c9eec1b040b9d1550add91fab56dfb2ac103a79
Sean Chou <sean1093@gmail.com>
2020-07-05 20:27:54 +0800

    Refine CICD process #31

--- script/deploy-web.sh
+++ script/deploy-web.sh
@@ -1,12 +1,9 @@
 echo "\033[32m Start to deploy web portal...\033[0m"
 echo "\033[32m Build react project\033[0m"
-cd ../app/src && yarn build
-echo "\033[32m Copy build file into firebase public folder\033[0m"
-cd ..
-cp -a dist/. ../public
+cd ../app && yarn build
 echo "\033[32m Firebase login\033[0m"
-firebase login
 cd ..
+firebase login
 echo "\033[32m Deploy firebase hosting\033[0m"
 firebase deploy --only hosting
 echo "\033[32m Deploy is done!\033[0m"
