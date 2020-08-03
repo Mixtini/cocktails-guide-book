f71c08ae1a1c59d8e9b2497ee9d9189b6bbc9784
Sean Chou <sean1093@gmail.com>
2020-08-03 22:15:54 +0800

    refactor search.js compoent and separate children components

---
+++ app/src/container/search/components/card.js
@@ -0,0 +1,48 @@
+// core
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// third party component
+import Button from '@material-ui/core/Button';
+
+// components
+import {
+    CardTitle,
+    ItemName,
+    IGPostCard,
+    Image
+} from '../search.css.js';
+
+// utils, config and assets
+import SEARCH_TEXT from '../../../assets/wording/search.json';
+
+const Card = ({ value }) => {
+    const { name: { zh, en }, igtoken } = value;
+    const igPostUrl = `https://www.instagram.com/p/${igtoken}`;
+    const url = `${igPostUrl}/media/?size=l`;
+    const onClickPost = () => {
+        window.open(igPostUrl, "_blank");
+    };
+    return (
+        <IGPostCard>
+            <CardTitle>
+                <ItemName>
+                    {`${zh} (${en})`}
+                </ItemName>
+                <Button
+                    variant="contained"
+                    onClick={onClickPost}
+                    color="primary"
+                >
+                    {SEARCH_TEXT.button.more}
+                </Button>
+            </CardTitle>
+            <Image url={url} onClick={onClickPost} />
+        </IGPostCard>
+    );
+};
+Card.propTypes = {
+    value: PropTypes.object.isRequired
+};
+
+export default Card;
---
+++ app/src/container/search/components/recommendBlock.js
@@ -0,0 +1,84 @@
+// core
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// third party component
+import Button from '@material-ui/core/Button';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+// components
+import {
+    StyledExpansionPanel,
+    OuterExpansionPanel,
+    OuterExpansionPanelSummary,
+    OuterExpansionPanelDetails,
+    InnerExpansionPanelSummary,
+    InnerExpansionPanelDetails,
+    StyledRecommendBlock,
+    ButtonGroup
+} from '../search.css.js';
+
+// utils, config and assets
+import SEARCH_TEXT from '../../../assets/wording/search.json';
+
+const RecommendBlock = ({ dataObj, expanded, onItemSelect, onExpanded, showRecommend, onControlRecommend }) => {
+    return (
+        <>
+            <StyledRecommendBlock>
+                <OuterExpansionPanel
+                    expanded={showRecommend}
+                    onChange={onControlRecommend}
+                >
+                    <OuterExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                        <div>{SEARCH_TEXT.recommend}</div>
+                    </OuterExpansionPanelSummary>
+                    <OuterExpansionPanelDetails>
+                    {
+                        showRecommend && dataObj.map((e) => {
+                            const { title, data } = e;
+                            return (
+                                <StyledExpansionPanel
+                                    expanded={expanded === title}
+                                    onChange={() => { onExpanded(title)} }
+                                    key={`item-${title}`}
+                                >
+                                    <InnerExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                        <div>{title}</div>
+                                    </InnerExpansionPanelSummary>
+                                    <InnerExpansionPanelDetails>
+                                        <ButtonGroup>
+                                            {
+                                                data.map((e, idx) => {
+                                                    return (
+                                                        <Button
+                                                            key={`key-${idx}`}
+                                                            variant="contained"
+                                                            onClick={() => { onItemSelect(e)} }
+                                                        >
+                                                            {e}
+                                                        </Button>
+                                                    );
+                                                })
+                                            }
+                                        </ButtonGroup>
+                                    </InnerExpansionPanelDetails>
+                                </StyledExpansionPanel>
+                            )
+                        })
+                    }   
+                    </OuterExpansionPanelDetails>
+                </ OuterExpansionPanel>
+            </StyledRecommendBlock>
+        </>
+    );
+};
+RecommendBlock.propTypes = {
+    dataObj: PropTypes.object.isRequired,
+    expanded: PropTypes.string.isRequired,
+    onItemSelect: PropTypes.func.isRequired,
+    onExpanded: PropTypes.func.isRequired,
+    showRecommend: PropTypes.bool.isRequired,
+    onControlRecommend: PropTypes.func.isRequired
+};
+
+export default RecommendBlock;
