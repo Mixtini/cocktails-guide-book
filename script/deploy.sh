a509f978b159477a1f6db4e1e37943fe904b3d18
Sean Chou <sean1093@gmail.com>
2020-08-26 21:45:30 +0800

    Migrate deploy script to package.json #50

--- script/deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "\033[32m Start to deploy web portal...\033[0m"
-echo "\033[32m Build react project\033[0m"
-cd ../app && yarn build
-echo "\033[32m Firebase login\033[0m"
-cd ..
-firebase login
-echo "\033[32m Deploy firebase hosting\033[0m"
-firebase deploy --only hosting
-echo "\033[32m Deploy is done!\033[0m"
